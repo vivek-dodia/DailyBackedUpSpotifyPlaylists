81670b8900dde13aca0fedb2a80ce284a5cfb20b
vivek-dodia <vivek.dodia@icloud.com>
2023-10-07 15:29:55 -0400

    Automated commit

--- TaskSchedular.bat
+++ TaskSchedular.bat
@@ -1,6 +1,9 @@
 @echo off
+:: Delete existing JSON files in the "playlists" folder
+del /Q playlists\22auvhdoqxfrv4t7stdgfxqha\.json
 cd /d "C:\Users\Vivek\Documents\spotify-backup-to-git"
 python script_json.py
+git pull origin main
 git add .
 git commit -m "Automated commit"
 git push origin main
