d5272273ba58db3860de9f2a35b551fa9c35fc92
vivek-dodia <vivek.dodia@icloud.com>
2023-10-07 15:56:10 -0400

    delete old json

--- TaskSchedular.bat
+++ TaskSchedular.bat
@@ -2,17 +2,30 @@
 :: Delete existing JSON files in the "playlists" subdirectory
 del /Q playlists\22auvhdoqxfrv4t7stdgfxqha\*.json
 
+:: Pull the main branch
 git pull origin main
 
+:: add all the folders that we want to push to git remote repo
 git add .
 
+:: just naming my commit for reference
 git commit -m "delete old json"
 
+:: pushing the changes with no old json files 
 git push origin main
 
+::directory for the python to generate the new json files for playlists
 cd /d "C:\Users\Vivek\Documents\spotify-backup-to-git"
 python script_json.py
+
+:: Pull the main branch
 git pull origin main
+
+:: add all the folders that we want to push to git remote repo
 git add .
-git commit -m "Automated commit"
+
+:: just naming my commit for reference
+git commit -m "Automated commit with updated playlists."
+
+:: pushing the changes with new json files 
 git push origin main
--- trigger.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call delete_json.bat
