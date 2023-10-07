1e19bca040137b753238ff23c8df9411c77a7d23
vivek-dodia <vivek.dodia@icloud.com>
2023-10-07 15:33:55 -0400

    Automated commit

--- script_json.py
+++ script_json.py
@@ -268,10 +268,6 @@ if __name__ == '__main__':
   main()
   t1_stop = perf_counter()
   print("Script completed in %.2f seconds" % (t1_stop - t1_start))
-  
-  # After saving the JSON files
-os.system('git add playlists/*')
-os.system('git commit -m "Update playlists"')
-os.system('git push origin main')
+
 
 print(f"Script completed in {perf_counter() - t1_start:.2f} seconds")
