036c947453bef4a783eb97b21dd51ca754eee652
vivek-dodia <vivek.dodia@icloud.com>
2023-10-07 15:50:32 -0400

    delete old json

--- TaskSchedular.bat
+++ TaskSchedular.bat
@@ -1,4 +1,15 @@
 @echo off
+:: Delete existing JSON files in the "playlists" subdirectory
+del /Q playlists\22auvhdoqxfrv4t7stdgfxqha\*.json
+
+git pull origin main
+
+git add .
+
+git commit -m "delete old json"
+
+git push origin main
+
 cd /d "C:\Users\Vivek\Documents\spotify-backup-to-git"
 python script_json.py
 git pull origin main
